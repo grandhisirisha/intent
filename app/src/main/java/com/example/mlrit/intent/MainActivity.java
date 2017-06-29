5f9b875becaa20b11021ec7648b24b66f0b85ab9
shirisha <nalla.shirisha@gmail.com>
2017-06-29 14:19:21 +0530

    Initial commit

---
+++ app/src/main/java/com/example/mlrit/intent/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.mlrit.intent;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+Button b1;
+    EditText et1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        b1=(Button)findViewById(R.id.button);
+        et1=(EditText)findViewById(R.id.editText);
+
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url=et1.getText().toString();
+                Intent intent=new Intent(Intent.ACTION_VIEW, Uri.parse(url));
+                startActivity(intent);
+            }
+        });
+    }
+}
